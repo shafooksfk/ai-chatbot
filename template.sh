f14f372723e343204307b0082a25b8b91bbf63ba
MUHAMMED SHAFOOK KELOTH <muhammed.shafook.keloth@redyoi.ai>
2025-10-01 16:58:57 +0400

    Folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/_init_.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and Files created successfully"
+
